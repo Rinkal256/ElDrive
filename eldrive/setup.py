d9eae75c240245c90f1ba1005be66895076056d5
Khadija Mahboob Alam <kha4408s@hs-coburg.de>
2025-12-14 19:45:30 +0100

    restructuring the repo

---
+++ eldrive/setup.py
@@ -0,0 +1,25 @@
+from setuptools import find_packages, setup
+
+package_name = 'eldrive'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='kdj',
+    maintainer_email='kha4408s@hs-coburg.de',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
